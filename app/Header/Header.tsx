27e8b4452f6bd452cc2b6cb2dc541ed0f841d6fb
israilkhan01 <israil.khan.a888@gmail.com>
2024-02-24 21:56:28 +0530

    added signup

--- app/Header/Header.tsx
+++ app/Header/Header.tsx
@@ -5,12 +5,14 @@ const Header = () => {
     return (
         <div className="w-full fixed left-0 top-0 z-10 flex items-center justify-between px-8 py-2 mb-[10px]">
             <div className="">
+                <Link href="/">
                 <Image
                     width={200}
                     height={50}
                     src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
                     alt="logo"
                 />
+                </Link>
             </div>
             <div className="text-yellow-500 text-lg font-bold flex align-middle">
                 <Link href="/login" >
