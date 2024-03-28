08ba509d2e57eb2b1043da387cdcd944aa040270
israilkhan01 <israil.khan.a888@gmail.com>
2024-03-29 00:51:42 +0530

    fixes

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -4,6 +4,6 @@ export const TMDB_API_OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZGRkNzgxMjZhZWJjMzMzMTcyNTM3NmY5ZDE1ZjQwZCIsInN1YiI6IjY2MDVhYWMyZmNlYzJlMDE4NmM1NDU2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n5Xw5lcdITcm2--0tgpJGhyF0euQCJ8fyR1Gqq8L2Zk'
+      Authorization: 'Bearer '+ `${process.env.TMDB_API_TOKEN}`
     }
   };
--- app/utils/customHooks/useNowPlayingMovies.ts
+++ app/utils/customHooks/useNowPlayingMovies.ts
@@ -1,3 +1,4 @@
+"use client"
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addNowPlayingMovies } from "../store/slices/movieSlices";
